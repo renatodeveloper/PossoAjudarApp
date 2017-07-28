080999ed2e580ab781780c0c4315967d4c8032b7
Renato <renato_developer@hotmail.com>
2017-07-28 16:54:47 -0300

    Implementação de RecyclerView.

--- app/src/main/java/com/possoajudar/app/application/ui/activities/MainActivity.java
+++ app/src/main/java/com/possoajudar/app/application/ui/activities/MainActivity.java
@@ -1,9 +1,13 @@
 package com.possoajudar.app.application.ui.activities;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,18 +17,49 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.possoajudar.app.R;
+import com.possoajudar.app.application.ui.recyclerview.MyRecyclerViewAdapter;
+import com.possoajudar.app.domain.model.Apontamento;
 import com.possoajudar.app.infrastructure.helper.ActivityUtil;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+
     public ActivityUtil activityUtil;
+    //*** - Implementação RecyclerView
+    private static RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private static RecyclerView recyclerView;
+    private static ArrayList<Apontamento> data;
+    public static View.OnClickListener myOnClickListener;
+    private static ArrayList<Integer> removedItems;
+    //*** - Implementação RecyclerView
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //menu ***
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+        //menu ***
+
+        //navigationView ***
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        /*
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -45,7 +80,33 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        */
 
+
+        //*** - Implementação RecyclerView
+        myOnClickListener = new MyOnClickListener(this);
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+
+
+        data = new ArrayList<Apontamento>();
+        for (int i = 0; i < Apontamento.getArrayDs().length; i++) {
+            data.add(new Apontamento(
+                    Apontamento.getArrayDs()[i],
+                    Apontamento.getArraySubDs()[i],
+                    Apontamento.getArrayId()[i],
+                    Apontamento.getArrayImgStatus()[i]
+            ));
+        }
+
+        removedItems = new ArrayList<Integer>();
+
+        adapter = new MyRecyclerViewAdapter(data);
+        recyclerView.setAdapter(adapter);
     }
 
     @Override
@@ -115,5 +176,37 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+    //*** - Implementação RecyclerView
+    private static class MyOnClickListener implements View.OnClickListener {
+
+        private final Context context;
+
+        private MyOnClickListener(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        public void onClick(View v) {
+            removeItem(v);
+        }
+
+        private void removeItem(View v) {
+            int selectedItemPosition = recyclerView.getChildPosition(v);
+            RecyclerView.ViewHolder viewHolder
+                    = recyclerView.findViewHolderForPosition(selectedItemPosition);
+            TextView textViewName
+                    = (TextView) viewHolder.itemView.findViewById(R.id.textViewDs);
+            String selectedName = (String) textViewName.getText();
+            int selectedItemId = -1;
+            for (int i = 0; i < Apontamento.getArrayDs().length; i++) {
+                if (selectedName.equals(Apontamento.getArrayDs()[i])) {
+                    selectedItemId = Apontamento.getArrayId()[i];
+                }
+            }
+            removedItems.add(selectedItemId);
+            data.remove(selectedItemPosition);
+            adapter.notifyItemRemoved(selectedItemPosition);
+        }
     }
 }
