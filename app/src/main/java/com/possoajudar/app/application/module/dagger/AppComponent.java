e88cab29cda021660f9500f024cda22b12dc3796
Renato <renato_developer@hotmail.com>
2017-07-28 10:02:35 -0300

    Dagger

--- app/src/main/java/com/possoajudar/app/application/module/dagger/AppComponent.java
+++ app/src/main/java/com/possoajudar/app/application/module/dagger/AppComponent.java
@@ -13,6 +13,7 @@ import javax.inject.Singleton;
 import dagger.Component;
 @Singleton
 @Component(modules = {AppModule.class})
+//@Component(modules = {AppModule.class, PresenterModule.class, NetworkModule.class})
 public interface AppComponent {
     void inject(Login target);
 }
