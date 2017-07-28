93b4db7abf3e26794a1a22767d888e3647a35782
Renato <renato_developer@hotmail.com>
2017-07-28 09:32:46 -0300

    Classe inicial que prover as instancias necessarias.

---
+++ app/src/main/java/com/possoajudar/app/application/module/dagger/AppModule.java
@@ -0,0 +1,25 @@
+package com.possoajudar.app.application.module.dagger;
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import com.possoajudar.app.domain.model.Apontamento;
+
+import javax.inject.Singleton;
+import dagger.Module;
+import dagger.Provides;
+/**
+ * Created by renato on 27/07/2017.
+ * Prover as instancias necessarias
+ */
+@Module
+public class AppModule {
+
+    @Provides
+    public Apontamento providesApontamento(){
+        return  new Apontamento("27-07-2017 as 14:47:02", "1.70 | 71 kg");
+    }
+
+}
+
