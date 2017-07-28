9e1a547b2af1bdae01f7173c0e0bed7368e28c97
Renato <renato_developer@hotmail.com>
2017-07-28 09:44:00 -0300

    Inicio dagger app extends application

---
+++ app/src/main/java/com/possoajudar/app/application/module/app/LoginApplication.java
@@ -0,0 +1,29 @@
+package com.possoajudar.app.application.module.app;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.possoajudar.app.application.module.dagger.AppComponent;
+import com.possoajudar.app.application.module.dagger.AppModule;
+import com.possoajudar.app.application.module.dagger.DaggerAppComponent;
+import com.possoajudar.app.application.ui.activities.Login;
+
+/**
+ * Created by renato on 27/07/2017.
+ */
+
+public class LoginApplication  extends Application {
+    private AppComponent appComponent;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        appComponent =  DaggerAppComponent.builder().appModule(new AppModule()).build();
+    }
+
+    public AppComponent getAppComponent() {
+
+        return appComponent;
+    }
+
+}
