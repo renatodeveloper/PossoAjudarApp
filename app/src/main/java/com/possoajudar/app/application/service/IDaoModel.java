4e9a6b0bc868c15d0f134c4b567105232169d5e6
Renato <renato_developer@hotmail.com>
2017-09-29 18:23:43 -0300

    Interface de contexto  a classe SQLiteOpenHelper.

---
+++ app/src/main/java/com/possoajudar/app/application/service/IDaoModel.java
@@ -0,0 +1,16 @@
+package com.possoajudar.app.application.service;
+
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by Renato on 29/09/2017.
+ */
+
+public interface IDaoModel {
+    SQLiteDatabase getdbInterno(SQLiteDatabase sqLiteDatabase);
+    SQLiteDatabase getdbExterno();
+
+    void showdbInternoError(int resId);
+    void showdbExternoError(int resId);
+    void startdbSucess();
+}
