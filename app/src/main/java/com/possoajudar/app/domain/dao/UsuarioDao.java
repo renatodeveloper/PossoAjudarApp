5a0f86dea863d778202b1c0fca24cbe0a436dd70
Renato <renato_developer@hotmail.com>
2017-09-29 19:21:20 -0300

    Usuário dao

---
+++ app/src/main/java/com/possoajudar/app/domain/dao/UsuarioDao.java
@@ -0,0 +1,10 @@
+package com.possoajudar.app.domain.dao;
+
+import com.possoajudar.app.domain.model.Usuario;
+
+/**
+ * Created by Renato on 27/09/2017.
+ */
+
+public class UsuarioDao extends Usuario {
+}
