c3a53d8a05afa5762e6506e14fa3602adb13f8d9
Renato <renato_developer@hotmail.com>
2017-09-29 19:20:19 -0300

    Classe bean Usuario

---
+++ app/src/main/java/com/possoajudar/app/domain/model/Usuario.java
@@ -0,0 +1,12 @@
+package com.possoajudar.app.domain.model;
+
+/**
+ * Created by Renato on 27/09/2017.
+ */
+
+public class Usuario {
+    int idUsuario;
+    String dsLogin;
+    String dsSenha;
+    int idRedeSocial;
+}
