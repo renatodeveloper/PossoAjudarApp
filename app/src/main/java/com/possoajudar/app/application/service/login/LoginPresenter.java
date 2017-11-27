912f97c0d9d2a57d3e274f8dc44743b57f33cf2a
Renato <renato_developer@hotmail.com>
2017-11-26 23:36:39 -0200

    ajustes

--- app/src/main/java/com/possoajudar/app/application/service/login/LoginPresenter.java
+++ app/src/main/java/com/possoajudar/app/application/service/login/LoginPresenter.java
@@ -1,17 +1,24 @@
 package com.possoajudar.app.application.service.login;
 
+import android.content.Context;
+
 import com.possoajudar.app.R;
 import com.possoajudar.app.application.service.ILoginView;
+import com.possoajudar.app.infrastructure.helper.ActivityUtil;
 
 /**
  * Created by renato on 06/07/2017.
  */
 
 public class LoginPresenter {
+    Context context;
+    ActivityUtil util;
     private ILoginView view;
     private LoginService service;
 
-    public LoginPresenter(ILoginView view, LoginService service){
+    public LoginPresenter(ILoginView view, LoginService service, Context context){
+        this.context = context;
+        util = new ActivityUtil();
         service = new LoginService();
         this.view = view;
         this.service = service;
@@ -32,7 +39,7 @@ public class LoginPresenter {
                 return;
             }
 
-            boolean loginSucceeded = service.login(username, password);
+            boolean loginSucceeded = service.login(util.getValeuJson(this.context, username, password), this.context);
             if(loginSucceeded){
                 view.startMainActivity();
             }else{
