b46a6a391a035ee7c221ae06ceec603a338f917f
Renato <renato_developer@hotmail.com>
2017-11-26 23:37:24 -0200

    Implementação.

--- app/src/main/java/com/possoajudar/app/application/service/login/LoginService.java
+++ app/src/main/java/com/possoajudar/app/application/service/login/LoginService.java
@@ -1,15 +1,36 @@
 package com.possoajudar.app.application.service.login;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.possoajudar.app.R;
+import com.possoajudar.app.application.service.dao.DaoModelService;
+
+import org.json.JSONObject;
+
 /**
  * Created by renato on 06/07/2017.
  */
 
 public class LoginService {
+    DaoModelService daoModelService;
 
-    public boolean login(String name, String password){
+    public boolean login(JSONObject jsonValueLogin, Context context){
         try{
-            if(name.equals("developer") && password.equals("developer")){
-                return  true;
+            SQLiteDatabase database =  daoModelService.getdbInterno(context);
+
+            if(database!= null && jsonValueLogin != null && jsonValueLogin.length()>0){
+                String[] args = { jsonValueLogin.getString(context.getString(R.string.dsGeneric_A)), jsonValueLogin.getString(context.getString(R.string.dsGeneric_B))};
+                Cursor cursor = database.query(context.getString(R.string.dsNameTblUser), null, "dsLogin=? AND dsSenha=?", args, null,null,null);
+                if(cursor.getCount()>0){
+                    cursor.moveToFirst();
+                    String dsLogin = cursor.getString(cursor.getColumnIndex(context.getString(R.string.dsLoginTblUser)));
+                    String dsSenha = cursor.getString(cursor.getColumnIndex(context.getString(R.string.dsSenhaTblUser)));
+                    if(dsLogin.length()>0 && dsSenha.length()>0){
+                        return  true;
+                    }
+                }
             }
         }catch (Exception e){
             e.getMessage().toString();
