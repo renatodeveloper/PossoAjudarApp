d3eaf62a44085104b43ba3114767b894ed77c511
renato <renato_developer@hotmail.com>
2017-12-31 19:33:59 -0200

    Interface pertinente ao apontamemto do usuário.

--- app/src/main/java/com/possoajudar/app/application/service/ICadApontamentoView.java
+++ app/src/main/java/com/possoajudar/app/application/service/ICadApontamentoView.java
@@ -1,5 +1,7 @@
 package com.possoajudar.app.application.service;
 
+import org.json.JSONArray;
+
 /**
  * Created by Renato on 03/12/2017.
  */
@@ -7,9 +9,15 @@ package com.possoajudar.app.application.service;
 public interface ICadApontamentoView {
     String getCadApontamentoAltura();
     String getCadApontamentoPeso();
+    String getCadApontamentoDsDataTime();
+    long   getCadApontamentoDataTime();
+    JSONArray montaListaApondatamento(JSONArray jsonArray);
+
 
     void showCadApontamentoAlturaError(int resId);
     void showCadApontamentoPesoError(int resId);
+    void showCadApontamentoDataTimeError(int resId);
     void showCadApontamentoError(int resId);
-    void startMainListActivity();
+    void showMontaListaApontamentoError(int resId);
+    //void startMainListActivity();
 }
