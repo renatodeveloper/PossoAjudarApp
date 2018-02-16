f9ebb0f8b5d1cb1e54b07af58324bc77fa079e9f
renato <renato_developer@hotmail.com>
2018-02-16 20:54:50 -0200

    Classe apenas para teste

---
+++ app/src/main/java/com/possoajudar/app/application/ui/activities/Dimensao.java
@@ -0,0 +1,20 @@
+package com.possoajudar.app.application.ui.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.possoajudar.app.R;
+
+/**
+ * Created by renato on 15/02/2018.
+ */
+
+public class Dimensao extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ly_dimensao);
+
+    }
+}
