8ddc8002dbccf0acbd951642455d5a05d4c9b6c3
renato <renato_developer@hotmail.com>
2018-03-14 17:56:54 -0300

    add método getByteArrayPhoto e remoção da persistencia da preferenes foto

--- app/src/main/java/com/possoajudar/app/application/service/cadastro/CadUserPresenter.java
+++ app/src/main/java/com/possoajudar/app/application/service/cadastro/CadUserPresenter.java
@@ -6,6 +6,9 @@ import com.possoajudar.app.R;
 import com.possoajudar.app.application.service.ICadUserView;
 import com.possoajudar.app.infrastructure.helper.ActivityUtil;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 /**
  * Created by Renato on 27/09/2017.
  */
@@ -37,11 +40,32 @@ public class CadUserPresenter {
                 return;
             }
 
-            boolean registerSucceeded = service.registerNewUser(util.getValeuJson(this.context, cadEmailUser, cadSenhaUser));
+            JSONObject object = new JSONObject();
+            object.put(context.getString(R.string.dsLoginTblUser), cadEmailUser);
+            object.put(context.getString(R.string.dsSenhaTblUser), cadSenhaUser);
+            object.put(context.getString(R.string.idTblServico), 1);
+            object.put(context.getString(R.string.idTblRedeSocial), 1);
+
+            boolean registerSucceeded = service.registerNewUser(object);
             if(registerSucceeded){
                 view.startMainActivity();
             }else{
                 view.showCadUserError(R.string.strLyCadastrofailed);
+            }
+
+        }catch (Exception e){
+            e.getMessage().toString();
+        }
+    }
+
+    public void getByteArrayPhoto(){
+        byte[] byteArray;
+        try{
+            byteArray = service.getBytePhoto();
+            if(byteArray != null && byteArray.length>0){
+                view.setByteArrayPhoto(byteArray);
+            }else{
+                view.nonePhoto();
             }
         }catch (Exception e){
             e.getMessage().toString();
