bfc6b598855bf05ffb6d5a4f25ea56792af36552
renato <renato_developer@hotmail.com>
2018-03-24 20:38:44 -0300

    AdMob by Google.

--- app/src/main/java/com/possoajudar/app/application/ui/activities/Login.java
+++ app/src/main/java/com/possoajudar/app/application/ui/activities/Login.java
@@ -9,6 +9,9 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 import com.possoajudar.app.BuildConfig;
 import com.possoajudar.app.R;
 //import com.possoajudar.app.application.module.app.LoginApplication;
@@ -54,6 +57,8 @@ public class Login extends Activity implements ILoginView {
     private LoginPresenter loginPresenter;
     public ActivityUtil activityUtil;
     GpsService gps;
+
+    private AdView mAdView;
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
@@ -120,6 +125,46 @@ public class Login extends Activity implements ILoginView {
             }
         });
          */
+
+
+        mAdView = (AdView) findViewById(R.id.adView);
+
+        AdRequest adRequest = new AdRequest.Builder().addTestDevice(AdRequest.DEVICE_ID_EMULATOR).build();
+        mAdView.loadAd(adRequest);
+
+        mAdView.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                // Code to be executed when an ad finishes loading.
+                //Toast.makeText(getApplicationContext(), "onAdLoaded...", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                // Code to be executed when an ad request fails.
+                //Toast.makeText(getApplicationContext(), "onAdFailedToLoad...", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onAdOpened() {
+                // Code to be executed when an ad opens an overlay that
+                // covers the screen.
+                //Toast.makeText(getApplicationContext(), "onAdOpened...", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onAdLeftApplication() {
+                // Code to be executed when the user has left the app.
+                //Toast.makeText(getApplicationContext(), "onAdLeftApplication...", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onAdClosed() {
+                // Code to be executed when when the user is about to return
+                // to the app after tapping on an ad.
+                //Toast.makeText(getApplicationContext(), "onAdClosed...", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     /* butterknife.OnClick
