eaec648bc2a23ac36eedfe08adcec6fa60c6a7fe
renato <renato_developer@hotmail.com>
2018-04-01 21:44:53 -0300

    Classe de retorno do mock-server.

---
+++ app/src/main/java/com/possoajudar/app/domain/model/ServiceResponse.java
@@ -0,0 +1,15 @@
+package com.possoajudar.app.domain.model;
+
+public class ServiceResponse {
+    public String code;
+    public String message;
+
+    public String getCode() {
+
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
