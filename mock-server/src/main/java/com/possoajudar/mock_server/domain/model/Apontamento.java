fd068d53ca2900f5855a091c36c20a660248d342
renato <renato_developer@hotmail.com>
2018-04-26 15:23:37 -0300

    Classe de retorno para endpoint appointments method

---
+++ mock-server/src/main/java/com/possoajudar/mock_server/domain/model/Apontamento.java
@@ -0,0 +1,12 @@
+package com.possoajudar.mock_server.domain.model;
+
+public class Apontamento {
+    public int idApontamento;
+    public int dataHora;
+    public String dsDataHora;
+    public String vlPeso;
+    public String vlAltura;
+    public float imc;
+    public String dsStatus;
+    public int idUsuario;
+}
