7589c0050be566b5cfa9fa18176cb7945632dc93
renato <renato_developer@hotmail.com>
2018-04-26 10:18:45 -0300

    consumindo mockserver

--- app/src/main/java/com/possoajudar/app/infrastructure/backend/ApiClient.java
+++ app/src/main/java/com/possoajudar/app/infrastructure/backend/ApiClient.java
@@ -14,7 +14,7 @@ public class ApiClient {
     public static Retrofit getClient() {
         if (retrofit==null) {
             retrofit = new Retrofit.Builder()
-                    .baseUrl(Constants.Headers.URL_GIT_HUB_API)
+                    .baseUrl(Constants.Headers.URL_JSON_MOCKSERVER)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
