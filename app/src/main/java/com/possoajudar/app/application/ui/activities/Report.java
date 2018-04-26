437c84c9d9fdc4a5f53e2cf93af2f6fb63481a3c
renato <renato_developer@hotmail.com>
2018-04-26 15:22:37 -0300

    consumo e exibição endpoint appointments

--- app/src/main/java/com/possoajudar/app/application/ui/activities/Report.java
+++ app/src/main/java/com/possoajudar/app/application/ui/activities/Report.java
@@ -4,8 +4,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -13,6 +15,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.possoajudar.app.R;
+import com.possoajudar.app.application.service.ICadApontamentoView;
 import com.possoajudar.app.application.ui.adapter.CustomAdapter;
 import com.possoajudar.app.domain.model.Apontamento;
 import com.possoajudar.app.domain.model.ServiceResponse;
@@ -20,6 +23,7 @@ import com.possoajudar.app.infrastructure.backend.ApiClient;
 import com.possoajudar.app.infrastructure.backend.ApiInterface;
 import com.possoajudar.app.infrastructure.helper.BaseActivity;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
@@ -28,7 +32,7 @@ import retrofit.Call;
 import retrofit.Callback;
 import retrofit.Retrofit;
 
-public class Report extends BaseActivity  {
+public class Report extends BaseActivity implements ICadApontamentoView {
     private static final String TAG = "MainActivity";
 
     private TextView thedate;
@@ -37,7 +41,7 @@ public class Report extends BaseActivity  {
     ListView listView;
     private static CustomAdapter adapter;
     ArrayList<Apontamento> myDataModels = null;
-
+    String date = "";
 
     @Override
     protected int getContentView() {
@@ -60,21 +64,41 @@ public class Report extends BaseActivity  {
 
 
         Intent incoming = getIntent();
-        String date = incoming.getStringExtra("date");//exemplo retorno error '2018/3/11'
+        date  = incoming.getStringExtra("date");//exemplo retorno error '2018/3/11'
         if(date != null && date.length()>0){
 
             new Thread(new Runnable() {
                 @Override
                 public void run() {
                     ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
-                    Call<ServiceResponse> call = apiService.getAuthMockServer("developer@possoajudar.com","dev");
-                    call.enqueue(new Callback<ServiceResponse>() {
+                    Call<ArrayList<Apontamento>> call = apiService.getAppointmentsMockServer(date);
+                    call.enqueue(new Callback<ArrayList<Apontamento>>() {
                         @Override
-                        public void onResponse(retrofit.Response<ServiceResponse> response, Retrofit retrofit) {
-                            String result = response.body().toString();
-                            thedate.setText(response.message().toString());
+                        public void onResponse(retrofit.Response<ArrayList<Apontamento>> response, Retrofit retrofit) {
+                            try{
+                                ArrayList<Apontamento> list = response.body();
+                                JSONArray jsonArray = new JSONArray();
+                                JSONObject object;
+                                if(response != null && list.size()>0){
+                                    for(int x=0;x<list.size(); x++){
+                                        object = new JSONObject();
+                                        object.put(getString(R.string.idTblUserAptmento), list.get(x).idApontamento);
+                                        object.put(getString(R.string.dataTimeTblUserAptmento),list.get(x).dataHora);
+                                        object.put(getString(R.string.dsDataTimeTblUserAptmento),list.get(x).dsDataHora);
+                                        object.put(getString(R.string.dsPesoTblUserAptmento),list.get(x).vlPeso);
+                                        object.put(getString(R.string.dsAlturaTblUserAptmento),list.get(x).vlAltura);
+                                        object.put(getString(R.string.imcTblUserAptmento),list.get(x).imc);
+                                        object.put(getString(R.string.dsStatusTblUserAptmento),list.get(x).dsStatus);
+                                        object.put(getString(R.string.idTblUser),list.get(x).idUsuario);
+                                        jsonArray.put(object);
+                                    }
+                                    montaListaApondatamento(jsonArray);
+                                }
+                                thedate.setText(date);
+                            }catch (Exception e){
+                                e.getMessage().toString();
+                            }
                         }
-
                         @Override
                         public void onFailure(Throwable t) {
                             Log.e(TAG, "Error occured while fetching post.");
@@ -108,11 +132,103 @@ public class Report extends BaseActivity  {
         }
 
          */
+        /*
+
 
         myDataModels.add(new Apontamento(10, 20, "dsDataHora", "vlPeso", "vlAltura", 0, "dsStatus", 1));
 
         adapter = new CustomAdapter(myDataModels, getApplicationContext());
 
         listView.setAdapter(adapter);
+        */
+    }
+
+    @Override
+    public String getCadApontamentoAltura() {
+        return null;
+    }
+
+    @Override
+    public String getCadApontamentoPeso() {
+        return null;
+    }
+
+    @Override
+    public String getCadApontamentoDsDataTime() {
+        return null;
+    }
+
+    @Override
+    public long getCadApontamentoDataTime() {
+        return 0;
+    }
+
+    @Override
+    public JSONArray montaListaApondatamento(JSONArray jsonArray) {
+        try{
+            try{
+                final ArrayList<Apontamento> myDataModels;
+                listView=(ListView)findViewById(R.id.myList);
+                myDataModels= new ArrayList<>();
+                for(int x= 0; x<jsonArray.length(); x++){
+                    JSONObject object = (JSONObject) jsonArray.get(x);
+                    int    idApontamento =  object.getInt(getString(R.string.idTblUserAptmento));
+                    int    dataHora      =  object.getInt(getString(R.string.dataTimeTblUserAptmento));
+                    String dsDataHora    =  object.getString(getString(R.string.dsDataTimeTblUserAptmento));
+                    String vlPeso        = object.getString(getString(R.string.dsPesoTblUserAptmento));
+                    String vlAltura      = object.getString(getString(R.string.dsAlturaTblUserAptmento));
+                    int    inc           = object.getInt(getString(R.string.imcTblUserAptmento));
+                    String dsStatus      = object.getString(getString(R.string.dsStatusTblUserAptmento));
+                    int    idUsuario     = object.getInt(getString(R.string.idTblUser));
+
+                    myDataModels.add(new Apontamento(idApontamento, dataHora, dsDataHora, vlPeso, vlAltura, Float.valueOf(inc), dsStatus, idUsuario));
+                }
+
+
+                adapter = new CustomAdapter(myDataModels, getApplicationContext());
+
+                listView.setAdapter(adapter);
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        Apontamento dataModel= myDataModels.get(position);
+                        Snackbar.make(view, dataModel.getDsStatus().toString(), Snackbar.LENGTH_LONG)
+                                .setAction("No action", null).show();
+                    }
+                });
+            }catch (Exception e){
+                e.getMessage().toString();
+            }
+            //Toast.makeText(this, "Total da lista de itens a ser exibido: " + String.valueOf(jsonArray.length()), Toast.LENGTH_LONG).show();
+            return null;
+        }catch (Exception e){
+            e.getMessage().toString();
+        }
+        return null;
+    }
+
+    @Override
+    public void showCadApontamentoAlturaError(int resId) {
+
+    }
+
+    @Override
+    public void showCadApontamentoPesoError(int resId) {
+
+    }
+
+    @Override
+    public void showCadApontamentoDataTimeError(int resId) {
+
+    }
+
+    @Override
+    public void showCadApontamentoError(int resId) {
+
+    }
+
+    @Override
+    public void showMontaListaApontamentoError(int resId) {
+
     }
 }
