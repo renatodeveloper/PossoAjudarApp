f52a4facbfbe2816be26f4cd56b99ab0c8cb2d72
renato <renato_developer@hotmail.com>
2018-04-26 15:21:47 -0300

    add method appointments

--- mock-server/src/main/java/com/possoajudar/mock_server/infrastructure/backend/MockResponseFactory.java
+++ mock-server/src/main/java/com/possoajudar/mock_server/infrastructure/backend/MockResponseFactory.java
@@ -2,7 +2,15 @@ package com.possoajudar.mock_server.infrastructure.backend;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.possoajudar.mock_server.domain.model.Apontamento;
 import com.possoajudar.mock_server.domain.model.ServiceResponse;
+
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class MockResponseFactory {
     private final Gson gson;
@@ -38,4 +46,29 @@ public class MockResponseFactory {
         serviceResponse.message = "Operação não autorizada";
         return gson.toJson(serviceResponse);
     }
+    public String appointments() {
+        long yourmilliseconds = System.currentTimeMillis();
+        SimpleDateFormat sdf = new SimpleDateFormat("MMM dd,yyyy HH:mm");
+        Date resultdate = new Date(yourmilliseconds);
+
+        Apontamento apontamento;
+        ArrayList<Apontamento> list = new ArrayList<>();
+
+        for(int x=1; x<11;x++){
+            apontamento = new Apontamento();
+
+            apontamento.idApontamento = x;
+            apontamento.dataHora = (int) System.currentTimeMillis();
+            apontamento.dsDataHora = sdf.format(resultdate);
+            apontamento.vlPeso = String.valueOf(70 + x);
+            apontamento.vlAltura = "1.75";
+            apontamento.imc = 2500;
+            apontamento.dsStatus = "Mockado";
+            apontamento.idUsuario = x;
+
+            list.add(apontamento);
+        }
+        return gson.toJson(list);
+    }
+
 }
