493afa0e4b0e97d3bc4cada68668a818c1420020
renato <renato_developer@hotmail.com>
2018-04-27 22:19:46 -0300

    add nome.

--- app/src/main/java/com/possoajudar/app/domain/model/Usuario.java
+++ app/src/main/java/com/possoajudar/app/domain/model/Usuario.java
@@ -8,6 +8,7 @@ import java.sql.Blob;
 
 public class Usuario {
     public int idUsuario;
+    public String dsNome;
     public String dsLogin;
     public String dsSenha;
     public int idRedeSocial;
