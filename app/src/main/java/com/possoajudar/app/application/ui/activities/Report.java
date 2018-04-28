b1d00cbc844f2273a5a96879e12be7f5861b44f5
renato <renato_developer@hotmail.com>
2018-04-27 21:30:28 -0300

    add method onKeyDown e showConnectionRefusedError

--- app/src/main/java/com/possoajudar/app/application/ui/activities/Report.java
+++ app/src/main/java/com/possoajudar/app/application/ui/activities/Report.java
@@ -1,11 +1,13 @@
 package com.possoajudar.app.application.ui.activities;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -16,6 +18,7 @@ import android.widget.Toast;
 
 import com.possoajudar.app.R;
 import com.possoajudar.app.application.service.ICadApontamentoView;
+import com.possoajudar.app.application.service.cadastro.CadApontamentoPresenter;
 import com.possoajudar.app.application.ui.adapter.CustomAdapter;
 import com.possoajudar.app.domain.model.Apontamento;
 import com.possoajudar.app.domain.model.ServiceResponse;
@@ -35,6 +38,7 @@ import retrofit.Retrofit;
 public class Report extends BaseActivity implements ICadApontamentoView {
     private static final String TAG = "MainActivity";
 
+    static public Context context;
     private TextView thedate;
     private ImageView viewGoCalendar;
 
@@ -42,6 +46,8 @@ public class Report extends BaseActivity implements ICadApontamentoView {
     private static CustomAdapter adapter;
     ArrayList<Apontamento> myDataModels = null;
     String date = "";
+
+    private CadApontamentoPresenter presenter;
 
     @Override
     protected int getContentView() {
@@ -63,49 +69,58 @@ public class Report extends BaseActivity implements ICadApontamentoView {
         });
 
 
+        presenter = new CadApontamentoPresenter(this, this);
+
+        context = getApplicationContext();
         Intent incoming = getIntent();
         date  = incoming.getStringExtra("date");//exemplo retorno error '2018/3/11'
         if(date != null && date.length()>0){
-
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
-                    Call<ArrayList<Apontamento>> call = apiService.getAppointmentsMockServer(date);
-                    call.enqueue(new Callback<ArrayList<Apontamento>>() {
-                        @Override
-                        public void onResponse(retrofit.Response<ArrayList<Apontamento>> response, Retrofit retrofit) {
-                            try{
-                                ArrayList<Apontamento> list = response.body();
-                                JSONArray jsonArray = new JSONArray();
-                                JSONObject object;
-                                if(response != null && list.size()>0){
-                                    for(int x=0;x<list.size(); x++){
-                                        object = new JSONObject();
-                                        object.put(getString(R.string.idTblUserAptmento), list.get(x).idApontamento);
-                                        object.put(getString(R.string.dataTimeTblUserAptmento),list.get(x).dataHora);
-                                        object.put(getString(R.string.dsDataTimeTblUserAptmento),list.get(x).dsDataHora);
-                                        object.put(getString(R.string.dsPesoTblUserAptmento),list.get(x).vlPeso);
-                                        object.put(getString(R.string.dsAlturaTblUserAptmento),list.get(x).vlAltura);
-                                        object.put(getString(R.string.imcTblUserAptmento),list.get(x).imc);
-                                        object.put(getString(R.string.dsStatusTblUserAptmento),list.get(x).dsStatus);
-                                        object.put(getString(R.string.idTblUser),list.get(x).idUsuario);
-                                        jsonArray.put(object);
+            try{
+                        ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
+                        Call<ArrayList<Apontamento>> call = apiService.getAppointmentsMockServer(date);
+                        call.enqueue(new Callback<ArrayList<Apontamento>>() {
+                            @Override
+                            public void onResponse(retrofit.Response<ArrayList<Apontamento>> response, Retrofit retrofit) {
+                                try {
+                                    if (response.body() != null) {
+                                        ArrayList<Apontamento> list = response.body();
+                                        JSONArray jsonArray = new JSONArray();
+                                        JSONObject object;
+                                        if (response != null && list.size() > 0) {
+                                            for (int x = 0; x < list.size(); x++) {
+                                                object = new JSONObject();
+                                                object.put(getString(R.string.idTblUserAptmento), list.get(x).idApontamento);
+                                                object.put(getString(R.string.dataTimeTblUserAptmento), list.get(x).dataHora);
+                                                object.put(getString(R.string.dsDataTimeTblUserAptmento), list.get(x).dsDataHora);
+                                                object.put(getString(R.string.dsPesoTblUserAptmento), list.get(x).vlPeso);
+                                                object.put(getString(R.string.dsAlturaTblUserAptmento), list.get(x).vlAltura);
+                                                object.put(getString(R.string.imcTblUserAptmento), list.get(x).imc);
+                                                object.put(getString(R.string.dsStatusTblUserAptmento), list.get(x).dsStatus);
+                                                object.put(getString(R.string.idTblUser), list.get(x).idUsuario);
+                                                jsonArray.put(object);
+                                            }
+                                            montaListaApondatamento(jsonArray);
+                                        }
+                                    }else{
+                                        showConnectionRefusedError(date);
                                     }
-                                    montaListaApondatamento(jsonArray);
+                                    thedate.setText(date);
+                                    date = "";
+                                }catch (Exception e){
+                                    e.getMessage().toString();
                                 }
-                                thedate.setText(date);
-                            }catch (Exception e){
-                                e.getMessage().toString();
                             }
-                        }
-                        @Override
-                        public void onFailure(Throwable t) {
-                            Log.e(TAG, "Error occured while fetching post.");
-                        }
-                    });
-                }
-            }).start();
+                            @Override
+                            public void onFailure(Throwable t) {
+                                //Connection refused
+                                showConnectionRefusedError(date);
+                                Log.e(TAG, "Error occured while fetching post.");
+                            }
+                        });
+
+            }catch (Exception e){
+                e.getMessage().toString();
+            }
         }
 
         thedate.setText(date);
@@ -208,6 +223,11 @@ public class Report extends BaseActivity implements ICadApontamentoView {
     }
 
     @Override
+    public void showMontaListaApontamentoSemApontamento(int resId) {
+        Toast.makeText(getApplicationContext(), getString(resId), Toast.LENGTH_LONG).show();
+    }
+
+    @Override
     public void showCadApontamentoAlturaError(int resId) {
 
     }
@@ -231,4 +251,25 @@ public class Report extends BaseActivity implements ICadApontamentoView {
     public void showMontaListaApontamentoError(int resId) {
 
     }
+
+    @Override
+    public void showConnectionRefusedError(String date) {
+        JSONArray jsonArray;
+        try{
+            presenter.getArrayApontamentoUser(date);
+        }catch (Exception e){
+            e.getMessage().toString();
+        }
+    }
+
+    //****************************************************** onKeyDown
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            finish();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
 }
