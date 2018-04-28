1cf4bafe3d580536b4b48d34c97327743ade3085
renato <renato_developer@hotmail.com>
2018-04-27 21:29:03 -0300

    Tratamento retorno value data.

--- app/src/main/java/com/possoajudar/app/application/ui/activities/CalendarActivity.java
+++ app/src/main/java/com/possoajudar/app/application/ui/activities/CalendarActivity.java
@@ -19,13 +19,25 @@ public class CalendarActivity extends AppCompatActivity {
         mCalendarView = (CalendarView) findViewById(R.id.simpleCalendarView);
         mCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
-            public void onSelectedDayChange(CalendarView CalendarView, int year, int month, int dayOfMonth) {
-                String date = year + "/" + month + "/"+ dayOfMonth ;
-                Log.d(TAG, "onSelectedDayChange: yyyy/mm/dd:" + date);
-                Intent intent = new Intent(CalendarActivity.this,Report.class);
-                intent.putExtra("date",date);
-                startActivity(intent);
-                finish();
+            public void onSelectedDayChange(CalendarView calendarView, int year, int month, int dayOfMonth) {
+                try{
+                    String date = "";
+                    String mes  = "";
+                    if(month == 0 || month == 1 || month == 2 || month == 3 || month == 4 || month == 5 || month == 6 || month == 7 || month == 8 ){
+                        mes = "0" + (month+1);
+                        date = dayOfMonth+ "/"+ mes+ "/"+year;
+                    }else{
+                        date = dayOfMonth+ "/"+(month+1)+ "/"+year;
+                    }
+
+                    Log.d(TAG, "onSelectedDayChange: yyyy/mm/dd:" + date);
+                    Intent intent = new Intent(CalendarActivity.this,Report.class);
+                    intent.putExtra("date",date);
+                    startActivity(intent);
+                    finish();
+                }catch (Exception e){
+                    e.getMessage().toString();
+                }
             }
         });
     }
