39a6dabaf6114a276197f832296397c94f0c3192
renato <renato_developer@hotmail.com>
2018-04-29 00:57:21 -0300

    Add minimizeApp

--- app/src/main/java/com/possoajudar/app/infrastructure/helper/ActivityUtil.java
+++ app/src/main/java/com/possoajudar/app/infrastructure/helper/ActivityUtil.java
@@ -774,4 +774,11 @@ Guarda status do apontamento do usuário
         }
         return result;
     }
+
+    public void minimizeApp(Context ctx) {
+        Intent startMain = new Intent(Intent.ACTION_MAIN);
+        startMain.addCategory(Intent.CATEGORY_HOME);
+        startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        ctx.startActivity(startMain);
+    }
 }
