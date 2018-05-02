330e4a77773f429bb0ec8cb94f02046812b56768
renato <renato_developer@hotmail.com>
2018-05-02 18:37:25 -0300

    Classe bean

--- app/src/main/java/com/possoajudar/app/domain/model/Usuario.java
+++ app/src/main/java/com/possoajudar/app/domain/model/Usuario.java
@@ -1,6 +1,9 @@
 package com.possoajudar.app.domain.model;
 
+import com.google.gson.annotations.SerializedName;
+
 import java.sql.Blob;
+import java.util.List;
 
 /**
  * Created by Renato on 27/09/2017.
---
+++ app/src/main/java/com/possoajudar/app/domain/model/UsuarioList.java
@@ -0,0 +1,11 @@
+package com.possoajudar.app.domain.model;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsuarioList {
+        public List<Usuario> usuarios;
+
+        public UsuarioList(){
+            usuarios = new ArrayList<Usuario>();
+        }
+}
